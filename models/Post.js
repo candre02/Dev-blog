f834a39ba34ea2db611f8c94e3448cab14027d41
Crystal Andre <81876258+candre02@users.noreply.github.com>
2021-12-24 17:31:46 -0600

    added some root, body, header in the style file under css. created models dir and two files under models, added come content under all three: imported files, created post and user models/tables/columns, also created index file under models added imported files and created associations. created a seeds dir and a index file

---
+++ models/Post.js
@@ -0,0 +1,23 @@
+// Import files
+const { Model, Datatypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our Post model
+class Post extends Model {}
+
+// create fields/columns for Post model
+Post.init(
+    {
+        id: {
+            type: Datatypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: Datatypes.STRING,
+            allowNull: false
+        },
+        
+    }
+)
