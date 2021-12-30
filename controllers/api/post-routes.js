2172039948f4c0c829caf92dc2e4d780dbfec4be
Crystal Andre <81876258+candre02@users.noreply.github.com>
2021-12-30 15:46:30 -0600

    created login handlebars, added two forms. created home handlebars added ordered list and list of post info. create update-post file under js added content, create a new post file under js dir new post is add something and the update post is to edit a post, also create a delete post under js dir added content in it, added routes for comment, dashboard, user, post, home and linked both index's to there appropriate routes order

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,100 @@
+// import files
+const router = require('express').Router();
+const sequelize = require('../../config/connection');
+const { User, Post, Comment } = require('../../models');
+
+// Get all posts
+router.get('/', (req, res) => {
+    console.log('======================');
+    Post.findAll({
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at'
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  // Get by a single id
+  router.post('/', (req, res) => {
+    if (req.session) {
+        Post.create({
+          title: req.body.title,
+          post_url: req.body.post_url,
+          user_id: req.session.user_id
+        })
+          .then(dbPostData => res.json(dbPostData))
+          .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+        }
+  });
+
+  // Update by its id
+router.put('/:id', (req, res) => {
+    Post.update(
+      {
+        title: req.body.title
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  // Delete by its id
+  router.delete('/:id', (req, res) => {
+    console.log('id', req.params.id);
+    Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  module.exports = router;
